c195a4654c9ee6da3eeafdcfe7b0a79047abf9eb
fabulousDesigns <designsfabulous8gmail.com>
2022-08-17 18:23:45 +0300

    index.php

--- index.php
+++ index.php
@@ -1,64 +1,64 @@
 <!DOCTYPE html>
 <html lang="en" class="h-100">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="" />
-    <title>Cargo Shippers & management</title>
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="" />
+  <title>Cargo Shippers & management</title>
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
       }
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
+    }
+  </style>
 
-    <!-- Custom styles for this template -->
-    <link href="css/cover.css" rel="stylesheet" />
-  </head>
-  <body class="d-flex h-100 text-center text-white">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-      <header class="mb-auto">
-        <div>
-          <h3 class="logo float-md-start mb-0">Cargo Shippers</h3>
-          <nav class="nav nav-masthead justify-content-center float-md-end">
-            <a class="nav-link" href="#">Home</a>
-            <a class="nav-link" href="#">Contact</a>
-          </nav>
-        </div>
-      </header>
+  <!-- Custom styles for this template -->
+  <link href="css/cover.css" rel="stylesheet" />
+</head>
 
-      <main class="px-3">
-        <h1>Global Cargo Shippers & Logistics</h1>
-        <p class="lead">
-         Welcome to the best Logistics solutions you can rely on! We manage your complex logistics issues so you can focus on
-                    building a stronger business, faster
-        </p>
-        <p class="lead">
-          <a
-            href="login.php"
-            class="btn-m btn-primary-m"
-            >Get Started</a
-          >
-        </p>
-      </main>
+<body class="d-flex h-100 text-center text-white">
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto">
+      <div>
+        <h3 class="logo float-md-start mb-0">Cargo Shippers</h3>
+        <nav class="nav nav-masthead justify-content-center float-md-end">
+          <a class="nav-link mx-2" href="index.php">Home</a>
+          <a class="nav-link mx-2" href="login.php">login</a>
+        </nav>
+      </div>
+    </header>
 
-      <footer class="mt-auto text-white-50">
-        <p>
-          Made with ❤ by
-          <a href="https://fabulousdesigns.github.io/my-website/" class="text-link-port">Fabulous Designs</a>,
-          &copy; 2022</a>.
-        </p>
-      </footer>
-    </div>
-  </body>
+    <main class="px-3">
+      <h1>Global Cargo Shippers & Logistics</h1>
+      <p class="lead">
+        Welcome to the best Logistics solutions you can rely on! We manage your complex logistics issues so you can focus on
+        building a stronger business, faster
+      </p>
+      <p class="lead">
+        <a href="login.php" class="btn-m btn-primary-m">Get Started</a>
+      </p>
+    </main>
+
+    <footer class="mt-auto text-white-50">
+      <p>
+        Made with ❤ by
+        <a href="https://fabulousdesigns.github.io/my-website/" class="text-link-port">Fabulous Designs</a>,
+        &copy; 2022</a>.
+      </p>
+    </footer>
+  </div>
+</body>
+
 </html>
