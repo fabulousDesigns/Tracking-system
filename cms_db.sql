ad9f9da87f40b05a4890bc988f17e11b7399a6a3
Bernard  Maina <87483225+fabulousDesigns@users.noreply.github.com>
2022-12-15 10:54:53 +0300

    Add files via upload

---
+++ cms_db.sql
@@ -0,0 +1,300 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2022 at 04:21 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branches`
+--
+
+CREATE TABLE `branches` (
+  `id` int(30) NOT NULL,
+  `branch_code` varchar(50) NOT NULL,
+  `street` text NOT NULL,
+  `city` text NOT NULL,
+  `state` text NOT NULL,
+  `zip_code` varchar(50) NOT NULL,
+  `country` text NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`id`, `branch_code`, `street`, `city`, `state`, `zip_code`, `country`, `contact`, `date_created`) VALUES
+(3, 'KyIab3mYBgAX71t', 'st. Ug', 'KAmpala', 'Kampala', '6000', 'Uganda', '+25571002345', '2020-11-26 16:45:05'),
+(4, 'dIbUK5mEh96f0Zc', 'St. Sample Arusha', 'Arusha', 'Arusha', '678543', 'Tanzania', '+2564998765', '2020-11-27 13:31:49'),
+(5, 'DYntKcIba7QSXq6', '2345678', 'zxcvbnm', 'asdfghj', '1234567', 'werty', '12345678', '2022-08-15 17:11:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `image_text` text NOT NULL,
+  `date_uploaded` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcels`
+--
+
+CREATE TABLE `parcels` (
+  `id` int(30) NOT NULL,
+  `reference_number` varchar(100) NOT NULL,
+  `sender_name` text NOT NULL,
+  `sender_address` text NOT NULL,
+  `sender_contact` text NOT NULL,
+  `recipient_name` text NOT NULL,
+  `recipient_address` text NOT NULL,
+  `recipient_contact` text NOT NULL,
+  `type` int(1) NOT NULL COMMENT '1 = Deliver, 2=Pickup',
+  `from_branch_id` varchar(30) NOT NULL,
+  `to_branch_id` varchar(30) NOT NULL,
+  `weight` varchar(100) NOT NULL,
+  `height` varchar(100) NOT NULL,
+  `width` varchar(100) NOT NULL,
+  `length` varchar(100) NOT NULL,
+  `price` float NOT NULL,
+  `status` int(2) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `parcels`
+--
+
+INSERT INTO `parcels` (`id`, `reference_number`, `sender_name`, `sender_address`, `sender_contact`, `recipient_name`, `recipient_address`, `recipient_contact`, `type`, `from_branch_id`, `to_branch_id`, `weight`, `height`, `width`, `length`, `price`, `status`, `date_created`) VALUES
+(8, '691664403618', 'huncho Huncho', '3422434224', '0110026199', 'barbara', '0987654321', '123456789', 1, '1', '', '2kg', '100cm', '300cm', '200cm', 3900, 5, '2022-07-27 13:36:34'),
+(9, '800886829042', 'wertyu', '746374', '0110026199', 'ghghdshg', '32525', '0110026199', 2, '4', '3', '34', '90', '67', '78', 800, 7, '2022-07-28 16:30:01'),
+(10, '254937344469', 'dedd', 'wwww', '0110026199', '3dededed', 'ededfefd', '0110026199', 1, '4', '', '21', '32', '45', '34', 600, 6, '2022-07-28 16:31:25'),
+(11, '456172787304', 'ben', '3456789', '0110026199', '456789', '34r5t6y7u8i', '0110026199', 2, '4', '4', '2345678', '345678', '45678', '4567', 45678, 7, '2022-07-31 09:31:48'),
+(12, '464932724279', 'asd', '123456', '1234567', 'werty', '234567', '23456', 2, '5', '5', '1234', '2345', '234', '2345', 1123460, 8, '2022-08-15 17:13:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcel_tracks`
+--
+
+CREATE TABLE `parcel_tracks` (
+  `id` int(30) NOT NULL,
+  `parcel_id` int(30) NOT NULL,
+  `status` int(2) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `parcel_tracks`
+--
+
+INSERT INTO `parcel_tracks` (`id`, `parcel_id`, `status`, `date_created`) VALUES
+(1, 2, 1, '2020-11-27 09:53:27'),
+(2, 3, 1, '2020-11-27 09:55:17'),
+(3, 1, 1, '2020-11-27 10:28:01'),
+(4, 1, 2, '2020-11-27 10:28:10'),
+(5, 1, 3, '2020-11-27 10:28:16'),
+(6, 1, 4, '2020-11-27 11:05:03'),
+(7, 1, 5, '2020-11-27 11:05:17'),
+(8, 1, 7, '2020-11-27 11:05:26'),
+(9, 3, 2, '2020-11-27 11:05:41'),
+(10, 6, 1, '2020-11-27 14:06:57'),
+(11, 7, 5, '2022-07-23 18:10:00'),
+(12, 7, 6, '2022-07-23 18:13:33'),
+(13, 7, 8, '2022-07-23 18:15:19'),
+(14, 6, 8, '2022-07-24 12:42:41'),
+(15, 8, 3, '2022-07-27 13:41:26'),
+(16, 11, 5, '2022-07-31 09:34:21'),
+(17, 11, 5, '2022-09-02 21:52:16'),
+(18, 8, 5, '2022-09-02 21:52:43'),
+(19, 11, 5, '2022-09-02 12:00:26'),
+(20, 11, 4, '2022-09-02 12:00:42'),
+(21, 12, 2, '2022-09-02 12:06:16'),
+(22, 12, 5, '2022-09-02 12:16:10'),
+(23, 11, 7, '2022-10-11 20:13:16'),
+(24, 10, 6, '2022-10-11 20:17:01'),
+(25, 9, 7, '2022-10-11 20:34:46'),
+(26, 12, 6, '2022-10-11 20:45:30'),
+(27, 12, 8, '2022-10-12 09:00:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shippers`
+--
+
+CREATE TABLE `shippers` (
+  `id` int(20) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last name` varchar(20) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `review` varchar(200) NOT NULL,
+  `date` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shippers`
+--
+
+INSERT INTO `shippers` (`id`, `first_name`, `last name`, `telephone`, `password`, `review`, `date`) VALUES
+(1, 'Fabulous', 'Designs', '0110026199', '11641999', '', '0000-00-00 00:00:00.000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Cargo Shippers & Logistics Management System', 'info@cargoshipperslogistics.com', '+254713 8542 623', '2102  Kenyatta Road, Nairobi, 3304', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1 = admin, 2 = staff',
+  `branch_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `branch_id`, `date_created`) VALUES
+(1, 'Maina', 'Mburu', 'damian@gmail.com', '8fd2f98802e036c6d3c604a5a3bb6418', 1, 0, '2020-11-26 10:57:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branches`
+--
+ALTER TABLE `branches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `parcels`
+--
+ALTER TABLE `parcels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `parcel_tracks`
+--
+ALTER TABLE `parcel_tracks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shippers`
+--
+ALTER TABLE `shippers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branches`
+--
+ALTER TABLE `branches`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `parcels`
+--
+ALTER TABLE `parcels`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `parcel_tracks`
+--
+ALTER TABLE `parcel_tracks`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `shippers`
+--
+ALTER TABLE `shippers`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
